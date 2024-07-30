3c97beb6acf24e298118f2bd572bff0253fafc12
S2orange <2244leemonk@gmail.com>
2024-07-30 08:43:23 +0000

    Pending changes exported from your codespace

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ BOT_TOKEN=("7124159999:AAEJ92t6POU88C8BYCOnihH-PUQfiesM5eA")
 OWNER_USERNAME=("Luis_enriquei")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5184635355")
+ADMIN_IDS=("5184635355 awa")
 
 
 
